9fb1d30d031e41ec84f030318d5ee3fb0cb3ac39
BolatMukashev <ya.ne.angel.kimi@gmail.com>
2025-02-25 17:51:17 +0500

    объединение и удаление

--- main.py
+++ main.py
@@ -25,8 +25,32 @@ def crop_pdfs(input_dir, output_dir, left, top, right, bottom):
             doc.close()
             print(f"Обрезан и сохранён: {output_path}")
 
+
+def merge_pdfs(input_dir, output_file):
+    pdf_merger = fitz.open()
+    
+    for filename in sorted(os.listdir(input_dir)):  # Сортировка для порядка
+        if filename.lower().endswith(".pdf"):
+            file_path = os.path.join(input_dir, filename)
+            pdf_merger.insert_pdf(fitz.open(file_path))
+            print(f"Добавлен: {file_path}")
+    
+    pdf_merger.save(output_file)
+    pdf_merger.close()
+    print(f"Объединённый файл сохранён как: {output_file}")
+
+
+def delete_non_qr_pdfs(directory):
+    for filename in os.listdir(directory):
+        if filename.lower().endswith(".pdf") and not filename.startswith("QR"):
+            file_path = os.path.join(directory, filename)
+            os.remove(file_path)
+            print(f"Удалён: {file_path}")
+
 if __name__ == "__main__":
     input_directory = r"C:\Users\Astana\Desktop\Client"  # Папка с исходными PDF
     output_directory = r"C:\Users\Astana\Desktop\Client\pdf_обрезанные"  # Папка для сохранения
     
     crop_pdfs(input_directory, output_directory, 0, 0, 290, 400)
+    merge_pdfs(output_directory, output_directory + "/" + "QR коды KASPI.pdf")
+    delete_non_qr_pdfs(output_directory)
